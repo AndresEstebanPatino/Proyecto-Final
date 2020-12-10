f4cbda230290a1d49a08a192a2656d7b0486d19e
strom99 <alissoncc17@gmail.com>
2020-12-10 10:09:54 +0100

    productosCarrito

--- ProyectoFinal/javascript.js
+++ ProyectoFinal/javascript.js
@@ -1,31 +1,19 @@
-const añadirCompras = document.querySelectorAll(".counterRight");
-const productos = [
-  {
-    precio: 52.0,
-    nombre: "Chicken Veggie Salad",
-    descripcion: "With Cheese souse",
-    cantidad: 0,
-  },
-  {
-    precio: 23.0,
-    nombre: "Mutton Curry",
-    descripcion: "With bastami rice",
-    cantidad: 0,
-  },
-  {
-    precio: 21.0,
-    nombre: "French fry",
-    descripcion: "With beef bacon",
-    cantidad: 0,
-  },
-];
 
+
+const productosCarrito = [];
+
+
+/*const nombre = 'Martin';
+// backticks
+const saludo = `Hola ${nombre}`*/
+
+// () [] {}
 function pintarProductos() {
   let listaPlatos = "";
   for (let i = 0; i < productos.length; i++) {
     listaPlatos += `
         <div class="infoPlatos">
-          <img src="./img/comida-rapida-casera.jpg" />
+          <img src="${productos[i].imagen}">
           <div class="infoDetalle">
             <p class="price">$${productos[i].precio}</p>
             <h4>${productos[i].nombre}</h4>
@@ -39,23 +27,319 @@ function pintarProductos() {
         </div>
     `;
   }
-  const platosContainer = document.querySelector(".platos");
-  platosContainer.innerHTML = listaPlatos;
+  const listaPlatosContenedor = document.querySelector('.platos');
+  listaPlatosContenedor.innerHTML = listaPlatos;
+  // document.querySelector('.platos').innerHTML = //listaPlatos;
 }
 
 pintarProductos();
 
-document.querySelector(".platos").addEventListener("click", function (event) {
-  const indice = event.target.dataset.id;
+let hola = document.querySelector(".platos");
+hola.addEventListener("click", addCantidad);
 
+function addCantidad (event) {
+  const indice = event.target.dataset.id;
   if (event.target.className === "counterLeft") {
     if (productos[indice].cantidad > 0) {
       productos[indice].cantidad--;
       pintarProductos();
+      productosCarrito[indice] = productos[indice];
     }
   }
   if (event.target.className === "counterRight") {
-    productos[indice].cantidad++;
-    pintarProductos();
+    if (productos[indice].cantidad < 3){
+      productos[indice].cantidad++;
+      pintarProductos();
+      productosCarrito[indice] = productos[indice];
+    }
+  } 
+  
+} 
+
+document.querySelector('.icoSend').addEventListener('click', nuevaPag);
+function nuevaPag(event) {
+  let listaCarrito = "";
+  for (let i = 0; i < productosCarrito.length; i++) {
+    listaCarrito += `
+        <div class="infoPlatos">
+          <img src="${productosCarrito[i].imagen}">
+          <div class="infoDetalle">
+            <p class="price">$${productosCarrito[i].precio}</p>
+            <h4>${productosCarrito[i].nombre}</h4>
+            <p>${productosCarrito[i].descripcion}</p>
+            <div class="counter">
+              <button data-id="${i}" class="counterLeft">-</button>
+              <p>${productosCarrito[i].cantidad}</p>
+              <button data-id="${i}" class="counterRight">+</button>
+            </div>
+          </div>
+        </div>
+    `;
   }
-});
+  document.getElementById('bodyHtml').innerHTML = listaCarrito;
+}
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
---
+++ ProyectoFinal/productos.js
@@ -0,0 +1,23 @@
+const productos = [
+    {
+      precio: 52.0,
+      nombre: "Chicken Veggie Salad",
+      descripcion: "With Cheese souse",
+      cantidad: 0,
+      imagen: "./img/comida-rapida-casera.jpg",
+    },
+    {
+      precio: 23.0,
+      nombre: "Mutton Curry",
+      descripcion: "With bastami rice",
+      cantidad: 0,
+      imagen: "./img/comida-rapida-casera.jpg",
+    },
+    {
+      precio: 21.0,
+      nombre: "French fry",
+      descripcion: "With beef bacon",
+      cantidad: 0,
+      imagen: "./img/comida-rapida-casera.jpg",
+    },
+  ];
