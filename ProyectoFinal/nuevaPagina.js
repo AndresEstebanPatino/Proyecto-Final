acd4ef811a768dea450e3b89e0ecf8aa339ec34e
Andres Patiño <andresp199519@gmail.com>
2020-12-11 12:33:21 +0100

    Actualización, se hio la funcion para añadir objetos al nuevo array carrito de compras

---
+++ ProyectoFinal/nuevaPagina.js
@@ -0,0 +1,51 @@
+
+let productosCarrito = [];
+
+document.querySelector('.icoSend').addEventListener('click', nuevaPag);
+
+function nuevaPag() {
+
+  let listaCarrito = "";
+  let añadirCarrito ="";
+  let headerCarrito= "";
+  añadirCarrito+=`<div id="carrito" class="carrito">
+                       <button  class="botonComprar"> < </button>
+                       </div>`;
+  headerCarrito+=`<div class="header">
+                      <button  class="botonRegreso" onclick="window.location.href='test.html'"> < </button>
+                      <h1 class="headerCarrito">My cart</h1>
+                     </div>`;
+                     
+
+    for (let i = 0; i < productosCarrito.length; i++) {
+
+      listaCarrito += `
+          <div class="infoPlatos">
+            <img src="${productosCarrito[i].imagen}">
+            <div class="infoDetalle">
+              <p class="price">$${productosCarrito[i].precio}</p>
+              <h4>${productosCarrito[i].nombre}</h4>
+              <p>${productosCarrito[i].descripcion}</p>
+              <div class="counter">
+                <button data-id="${i}" class="counterLeft">-</button>
+                <p>${productosCarrito[i].cantidad}</p>
+                <button data-id="${i}" class="counterRight">+</button>
+              </div>
+            </div>
+          </div>
+      `;  
+
+    }
+
+
+  document.querySelector('.slider').innerHTML = listaCarrito;
+  document.getElementById('header').innerHTML = headerCarrito;
+  document.getElementById('footer').innerHTML = añadirCarrito;
+
+ 
+  console.log(productosCarrito)
+    
+
+}
+
+
---
+++ ProyectoFinal/pintarProductos.js
@@ -0,0 +1,25 @@
+function pintarProductos() {
+    let listaPlatos = "";
+    for (let i = 0; i < productos.length; i++) {
+      listaPlatos += `
+          <div class="infoPlatos">
+            <img src="${productos[i].imagen}">
+            <div class="infoDetalle">
+              <p class="price">$${productos[i].precio}</p>
+              <h4>${productos[i].nombre}</h4>
+              <p>${productos[i].descripcion}</p>
+              <div class="counter">
+                <button data-id="${i}" class="counterLeft">-</button>
+                <p>${productos[i].cantidad}</p>
+                <button data-id="${i}" class="counterRight">+</button>
+              </div>
+            </div>
+          </div>
+      `;
+  
+    }
+    const listaPlatosContenedor = document.querySelector('.platos');
+    document.querySelector('.platos').innerHTML = listaPlatos;
+  }
+
+  pintarProductos();
